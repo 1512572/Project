05b7f22e910f182e8e7aa31dcaefbb8417ef8905
1512572 <35843568+1512572@users.noreply.github.com>
2018-04-11 23:19:50 +0700

    Add login.html

--- js/javascript.js
+++ js/javascript.js
@@ -8,3 +8,14 @@ $(window).scroll(function(e){
     parallax();
 });
 
+$('#signup-page-btn').click(function(e){
+    $('#login-form').fadeOut('fast', function(){
+        $('#signup-form').fadeIn('fast');
+    });
+});
+
+$('#login-page-btn').click(function(e){
+    $('#signup-form').fadeOut('fast', function(){
+        $('#login-form').fadeIn('fast');
+    });
+});
