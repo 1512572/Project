c579af87b7a68684a623394581f612649321b3c7
1512572 <35843568+1512572@users.noreply.github.com>
2018-04-14 21:40:07 +0700

    Javascript update
    Category JS

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,12 @@
-var jumboHeight = $('.jumbotron').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
-}
-
-$(window).scroll(function(e){
-    parallax();
-});
-
-$('#signup-page-btn').click(function(e){
-    $('#login-form').fadeOut('fast', function(){
+$('#signup-page-btn').click(function (e) {
+    $('#login-form').fadeOut('fast', function () {
         $('#signup-form').fadeIn('fast');
     });
 });
 
-$('#login-page-btn').click(function(e){
-    $('#signup-form').fadeOut('fast', function(){
+$('#login-page-btn').click(function (e) {
+    $('#signup-form').fadeOut('fast', function () {
         $('#login-form').fadeIn('fast');
     });
 });
+
