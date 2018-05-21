8d6a6474934fd6837faf5c54fd2c4f3e4d131c9f
xuantruonghmd96 <xuantruonghmd96@gmail.com>
2018-05-21 15:08:08 +0700

    sort table by clicking the headers on admin pages, price-slider and interface sort product by price on category page

--- js/javascript.js
+++ js/javascript.js
@@ -10,3 +10,27 @@ $('#login-page-btn').click(function (e) {
     });
 });
 
+$(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+        show: {
+            //effect: "slideDown",
+            delay: 500
+        },
+        track: true
+    });
+});
+
+// script for #filterProduct-slider of price
+$(function () {
+    $("#slider-range-price-category").slider({
+        range: true,
+        min: 0,
+        max: 1000000,
+        values: [0, 1000000],
+        slide: function (event, ui) {
+            $("#amount").val(ui.values[0] + " - " + ui.values[1]);
+        }
+    });
+    $("#amount").val($("#slider-range-price-category").slider("values", 0) +
+        " - " + $("#slider-range-price-category").slider("values", 1));
+});
