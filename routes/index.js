da843e03a441f46150c5eebe9df92d177642fd9f
1512572 <35843568+1512572@users.noreply.github.com>
2018-06-03 00:38:19 +0700

    Product filter & sort rules added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
-var router = express.Router();
+var bodyParser = require('body-parser');
 var Product = require('../models/product');
+
+var router = express.Router();
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 
 /* GET home page. */
@@ -14,22 +21,115 @@ router.get('/shop', function (req, res, next) {
   Product.find(function (err, docs) {
     var chunkSize = 4;
     var productChunks = [];
-    for (var i = 0; i < docs.length; i += chunkSize){
+    for (var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i + chunkSize));
     }
-    res.render('shop/shop', {
-      title: 'Shop',
-      products: productChunks
+    Product.find().distinct('material', function (err, docs) {
+      res.render('shop/shop', {
+        title: 'Shop',
+        products: productChunks,
+        matList: docs
+      });
     });
   });
 });
 
-router.get('/product/:id', function(req, res, next){
+router.post('/shop', function (req, res, next) {
+  var mat = req.body.material;
+  var sortBy = req.body.sortBy;
+  var order = req.body.order;
+  console.log(order);
+  if (mat === 'all') {
+    if (sortBy === 'none')
+      return Product.find(function (err, docs) {
+        var chunkSize = 4;
+        var productChunks = [];
+        for (var i = 0; i < docs.length; i += chunkSize) {
+          productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        Product.find().distinct('material', function (err, docs) {
+          res.render('shop/shop', {
+            title: 'Shop',
+            products: productChunks,
+            matList: docs
+          });
+        });
+      });
+    else
+      return Product.find().sort([
+        [sortBy, order]
+      ]).exec(function (err, docs) {
+        var chunkSize = 4;
+        var productChunks = [];
+        for (var i = 0; i < docs.length; i += chunkSize) {
+          productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        Product.find().distinct('material', function (err, docs) {
+          res.render('shop/shop', {
+            title: 'Shop',
+            products: productChunks,
+            matList: docs
+          });
+        });
+      });
+  } else {
+    if (sortBy === 'none')
+      return Product.find({
+        material: mat
+      }, function (err, docs) {
+        var chunkSize = 4;
+        var productChunks = [];
+        for (var i = 0; i < docs.length; i += chunkSize) {
+          productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        Product.find().distinct('material', function (err, docs) {
+          res.render('shop/shop', {
+            title: 'Shop',
+            products: productChunks,
+            matList: docs
+          });
+        });
+      });
+    else
+      return Product.find({
+        material: mat
+      }).sort([
+        [sortBy, order]
+      ]).exec(function (err, docs) {
+        var chunkSize = 4;
+        var productChunks = [];
+        for (var i = 0; i < docs.length; i += chunkSize) {
+          productChunks.push(docs.slice(i, i + chunkSize));
+        }
+        Product.find().distinct('material', function (err, docs) {
+          res.render('shop/shop', {
+            title: 'Shop',
+            products: productChunks,
+            matList: docs
+          });
+        });
+      });
+  }
+});
+
+router.get('/product/:id', function (req, res, next) {
   var id = req.params.id;
-  Product.findOne({ _id: id, status: true }, function(err, docs){
-    res.render('shop/product', {product: docs, err: err});
+  Product.findOne({
+    _id: id,
+    status: true
+  }, function (err, docs) {
+    if (err) {
+      return res.render('shop/product', {
+        title: 'Lỗi! ',
+        err: err
+      });
+    }
+    res.render('shop/product', {
+      title: 'Sản phẩm ' + docs.name,
+      product: docs,
+    });
   });
-  
+
 });
 
 module.exports = router;
